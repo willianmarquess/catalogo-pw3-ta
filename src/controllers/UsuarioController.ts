bbf698f7e90717218b46134c5fd8f6b1a9fbd9f0
Willian Marques <willian.goncalves4@gmail.com>
2026-02-26 21:29:57 -0300

    feat: class 16/02

--- src/controllers/UsuarioController.ts
+++ src/controllers/UsuarioController.ts
@@ -123,6 +123,29 @@ export class UsuarioController {
             return res.redirect('/usuario/logar');
         }
 
-        res.render('pages/usuario/perfil', { usuario, titulo: 'Perfil' });
+        res.render('pages/usuario/perfil', { usuario: usuarioEncontrado, titulo: 'Perfil' });
+    }
+
+    static async atualizarPerfil(req: Request, res: Response) {
+        const { usuario } = req.session as any;
+        const { nome, email, senha } = req.body;
+
+        const usuarioEncontrado = await Usuario.buscarPorId(usuario.id);
+        
+        if(!usuarioEncontrado) {
+            return res.redirect('/usuario/logar');
+        }
+
+        if(!nome || !email || !senha) {
+            return res.render('pages/usuario/perfil', {
+                usuario: usuarioEncontrado,
+                titulo: 'Perfil',
+                mensagem: {
+                    tipo: 'error',
+                    valor: 'Preencha todos os campos corretamente',
+                    titulo: 'Dados inválidos'
+                }
+            });
+        }
     }
 } 
